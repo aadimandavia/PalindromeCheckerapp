a272e489eca599ccf498e9d69cff4220a910f284
aadimandavia <aadim2612@gmail.com>
2026-02-27 10:00:21 +0530

    UC2 HardCode Palindrome

--- App/src/PalindromeCheckerapp.java
+++ App/src/PalindromeCheckerapp.java
@@ -2,7 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PalindromeCheckerapp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version: 1.0");
+
+        // Stores a predefined string
+        String input = "madam";
+
+        System.out.println("Input text: " + input);
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
     }
 }
